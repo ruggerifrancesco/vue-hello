e4d5aa41bfd4e4e9ee369b34f4252ad3e43b971f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-01 15:30:23 +0200

    Added instructions for importing via es6

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ createApp({
         }
     }
 }).mount('#app')
+
+
+// Inport CDN Via ES6
+// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+// createApp({
+//   data() {
+//     return {
+//       message: 'Hello Vue!'
+//     }
+//   }
+// }).mount('#app')
+
+// Remember that the script in the DOM will be positioned 
+// at the top, and have to be a type attribute module
+// <script type="module">
